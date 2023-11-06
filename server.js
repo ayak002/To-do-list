c02515c2ea768eda1f3893f9406c9701278863ac
ayak002 <aya.khadoum@epitech.eu>
2023-11-06 15:19:33 +0100

    changed port and added process.env.port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.port;
 
 app.use(express.json());
 
